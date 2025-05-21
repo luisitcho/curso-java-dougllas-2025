dbf83b858761d1adea6a05da0a5635c2ab9ad5f0
luisitcho <luishenrisc1@gmail.com>
2025-05-21 00:24:06 -0300

    curso java spring boot - Persistindo produtos no banco de dados

--- src/main/java/com/example/demo/repository/ProdutoRepository.java
+++ src/main/java/com/example/demo/repository/ProdutoRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
 
-public interface ProdutoRepository {
+import com.example.demo.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
 }
